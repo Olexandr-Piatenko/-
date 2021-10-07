75c15c4c09da7de7471412b38790fda9571930c2
Piatenko-ol <piatenko.web@gmail.com>
2021-10-07 14:57:37 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function(){
+
+$(document).ready(function() { 
+  var button = $('#button-up');	
+  $(window).scroll (function () {
+    if ($(this).scrollTop () > 300) {
+      button.fadeIn();
+    } else {
+      button.fadeOut();
+    }
+});	 
+button.on('click', function(){
+$('body, html').animate({
+scrollTop: 0
+}, 800);
+return false;
+});		 
+});
+   $('.menu__btn').on('click', function(){
+      $('.menu__list').toggleClass('menu__list--active');
+   })
+});
